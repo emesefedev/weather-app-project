cbf99aab88310ad7b6b4ce2d519a4d1fde032ede
emesefedev <emesefedev@gmail.com>
2024-10-16 15:29:59 +0200

    feature: get data from api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import "./style.css";
 
-console.log("Hello Weather");
+const key = "XVF7TBHT2V3J5DLP6WLJCBJVE";
+const inca = "39.72196,2.91347";
+const mancor = "39.75021,2.87229";
+const palma = "39.57562,2.65355";
+const sierraNevada = "37.09792,-3.39954";
+
+async function fetchWeatherTodayData(location) {
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/today?unitGroup=metric&key=${key}`;
+
+  const rawData = await fetch(url);
+  const data = await rawData.json();
+  return data.days[0];
+}
+
+const requestedData = await fetchWeatherTodayData(sierraNevada);
+console.log(requestedData.description);
+
+// Me interesa enseñar
+// * sunrise
+// * sunset
+// * moonphase
+// * description
+// * tempmax
+// * tempmin
+// * temp
+// * precip
+// * precipprob
+// * preciptype
+// * snow
+// * snowdepth
+// * winddir
+// * windspeed
+// * cloudcover
+// * humidity
